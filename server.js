7729801d20dbda93bfaf148c566d19ca1442889b
Ahasanul-Islam <ahasanuliabir150@gmail.com>
2023-08-28 20:55:27 +0600

    add and changes in server file

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ import authRouter from './routes/authRoutes.js';
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from 'path';
+import {fileURLToPath} from 'url';
 
 //config env
 dotenv.config();
 
 //databse config
 connectDB();
+
+//esmodule-file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
 
 
 //rest object
@@ -22,11 +28,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './client/build')))
 
-//rest api
-app.get("/" ,(req, res )=>{
-    res.send('wellcome');
-});
+// rest api
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 //routh
 app.use('/api/v1/auth', authRouter);
